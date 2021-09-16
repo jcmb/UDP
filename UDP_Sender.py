b3fe5d40f7743f96bb23e33f053e4e0cb13d3879
Geoffrey Kirk <GeoffreyKirk@Gmail.com>
2021-09-16 12:35:23 -0600

    Update UDP_Sender.py

--- UDP_Sender.py
+++ UDP_Sender.py
@@ -12,9 +12,9 @@ epilog="V1.0 (c) JCMBsoft 2016");
 parser.add_argument("-I","--Destination_IP",default="255.255.255.255",help="Source of UDP packets, use 255.255.255.255 for broadcast")
 parser.add_argument("-P","--Destination_Port",default="2103",help="Source port of UDP packets. Default 2103",type=int)
 parser.add_argument("-T","--Tell", action='store_true',help="Tell the settings before starting")
-parser.add_argument("-W","--Wait", default=5.0  ,help="Amount to delay before sending the next packet, in seconds, decimals delays supported. Default 1.0",type=float)
+parser.add_argument("-W","--Wait", default=5.0  ,help="Amount to delay before sending the next packet, in seconds, decimals delays supported. Default 5.0",type=float)
 parser.add_argument("-V","--Verbose", action='count',help="Display information on incomming -v, and outgoing -vv packets")
-parser.add_argument("-S","--Status", action='store_true',help="Display a . for each incomming packet")
+parser.add_argument("-S","--Status", action='store_true',help="Display a . for each incomming packet. Otherwise print the data")
 
 args = parser.parse_args()
 
@@ -40,8 +40,11 @@ if UDP_TRAN_IP=="255.255.255.255":
       sys.stderr.write("Outputting on broadcast address\n\n")
 
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.setblocking(0)
 
 Packets_Out=0
+Packets_In=0
+
 start_time=datetime.datetime.now()
 while True:
    try:
@@ -50,11 +53,21 @@ while True:
       Packets_Out+=1
       if Verbose:
          sys.stderr.write("Sent message # " + str (Packets_Out) + " : "+MESSAGE)
-      else:
-         if Dots:
-            sys.stderr.write(".")
-            if Packets_Out % 100==0:
-               sys.stderr.write ("\n")
+
+      try:
+         while True:
+            data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+            Packets_In+=1
+            if Dots:
+               sys.stderr.write(".")
+               if Packets_In % 100==0:
+                   sys.stderr.write ("\n")
+            else:
+               print("received message: %s" % data)
+
+
+      except socket.error:
+          pass
       sleep(Packet_Every)
    except KeyboardInterrupt:
       print ""
